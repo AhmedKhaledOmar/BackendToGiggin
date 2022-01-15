0d77fca30c94e05c140638bbaca07b01f574de98
AhmedKhaledOmar <ahmedkhaledomar22@gmail.com>
2022-01-15 12:23:20 +0200

    Install Frame work and Create DataBase In proccess

---
+++ BackendToGiggin/Models/GigginContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendToGiggin.Models
+{
+    public class GigginContext
+    {
+    }
+}
